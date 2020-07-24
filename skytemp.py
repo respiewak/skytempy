256b0b361f7e1e9f29c8b0075a4003b69c62a6e4
Renee Spiewak <respiewak@gmail.com>
2020-07-24 19:15:54 +1000

    Fixed the coordinate transforms! Now requires astropy-healpix installed

--- skytemp.py
+++ skytemp.py
@@ -3,6 +3,8 @@ from __future__ import print_function, division, absolute_import
 import numpy as np, argparse as ap
 from scipy.interpolate import CubicSpline
 from astropy.table import Table
+import astropy.units as u
+from astropy_healpix import HEALPix
 
 
 def proc_args():
@@ -17,84 +19,85 @@ def proc_args():
 
 
 class SkyTemp:
-    def __init__(self, gl, gb, table):
+    """
+    Class defining the radio temperature of the sky at a given position.
+    Designed to use the Remazeilles et al. 2014 reprocessing maps, or
+    others with similar structure (Healpix pixellation).
+
+    """
+
+    def __init__(self, gl, gb, fits):
+        """
+        Parameters
+        ----------
+        gl - float, Galactic longitude
+        gb - float, Galactic latitude
+        fits - str, path to fits file containing map
+
+        Useful attributes
+        -----------------
+        temp408 - float, the temperature of the sky position at 408 MHz
+
+        """
+
         self.gl = gl
         self.gb = gb
-        self.table = table
+        self.fits_file = fits
+        self.table, self.meta = self.read_fits()
+        self.flat_tab = self.table.flatten()
         self.temp408 = self.interp()
-        
-    def get_pix(self):
-        ylen, xlen = self.table.shape 
-        xval = np.floor(xlen*self.gl/360) 
-        yval = np.floor(ylen*(90-self.gb)/180) 
-        return(int(xval), int(yval))
-        
-    def get_glgb(self, xval, yval):
-        ylen, xlen = self.table.shape 
-        gl = 360*xval/xlen 
-        gb = 90 - (180*yval/ylen) 
-        return(gl, gb)
-        
+
+    def read_fits(self):
+        t = Table.read(self.fits_file)
+        return(t['TEMPERATURE'], t.meta)
+
+    def get_pix(self, gl, gb):
+        hp = HEALPix(nside=self.meta["NSIDE"],
+                     order=self.meta["ORDERING"])
+        return(hp.lonlat_to_healpix(gl*u.deg, gb*u.deg))
+
     def interp(self):
         gl = self.gl
         gb = self.gb
-        xcen, ycen = self.get_pix()
-        xvals = np.array([xcen-1, xcen, xcen+1])
-        yvals = np.array([ycen-1, ycen, ycen+1])
-        xwrap = None 
-        ywrap = None 
-        if xcen - 1 < 0 or xcen + 1 > self.table.shape[1]: 
-            xwrap = True 
-            xvals = xvals % self.table.shape[1] 
-             
-        x_fory = np.array([xcen, xcen, xcen]) 
-        if ycen - 1 < 0: 
-            ywrap = 'neg' 
-            yvals[0] = ycen 
-            x_fory[0] = (xcen+self.table.shape[1])%self.table.shape[1] 
-        elif ycen + 1 > self.table.shape[0]: 
-            ywrap = 'pos' 
-            yvals[-1] = ycen 
-            x_fory[-1] = (xcen+self.table.shape[1])%self.table.shape[1] 
-       
-        temps_l = np.array([self.table[ycen, xv] for xv in xvals]) 
-        temps_b = np.array([self.table[yv, xv] for xv, yv in zip(x_fory, yvals)]) 
-        if ywrap == 'neg': 
-            yvals[0] -= 1 
-        elif ywrap == 'pos': 
-            yvals[-1] += 1 
-            
-        lvals = np.zeros(3) 
-        bvals = np.zeros(3) 
-        for i, x, y in zip(np.arange(3), xvals, yvals): 
-            lvals[i], bvals[i] = self.get_glgb(x, y)
-      
-        if xwrap: 
-            lvals = ((lvals + 180) % 360) + 180 
-            gl = ((gl + 180) % 360) + 180 
-        
-        temps_l = temps_l[np.argsort(lvals)] 
-        lvals = lvals[np.argsort(lvals)] 
-        temps_b = temps_b[np.argsort(bvals)] 
-        bvals = bvals[np.argsort(bvals)] 
-        cs_l = CubicSpline(lvals, temps_l) 
-        cs_b = CubicSpline(bvals, temps_b) 
+        # Want a few nearby values separated by beamsize
+        beamsize = self.meta["BEAMSIZE"]/60
+        lvals = np.array([gl-beamsize, gl, gl+beamsize])
+        bvals = np.array([gb-beamsize, gb, gb+beamsize])
+        xvals = self.get_pix(lvals, np.array([gb, gb, gb]))
+        yvals = self.get_pix(np.array([gl, gl, gl]), bvals)
+        temps_l = self.flat_tab[xvals]
+        temps_b = self.flat_tab[yvals]
+
+        cs_l = CubicSpline(lvals, temps_l)
+        cs_b = CubicSpline(bvals, temps_b)
         return(np.mean([cs_l(gl), cs_b(gb)]))
-        
+
     def get_temp(self, freq, spec_ind=-2.9):
+        """
+        Derive the temperature of the sky at the given frequency.
+
+        Parameters
+        ----------
+        freq - float or numpy.ndarray of floats, the desired frequency/ies
+            for conversion
+        spec_ind - float, the spectral index of the sky temperature
+            (Default: -2.9)
+
+        Returns
+        -------
+        float or numpy.ndarray of floats (with the shape of `freq`)
+            representing the temperature at the given frequency/ies
+
+        """
+
         return(self.temp408*(freq/408)**spec_ind)
 
 
-def read_fits(fits_file):
-    t = Table.read(fits_file)
-    return(t['TEMPERATURE'])
-
-
 def main(fits, gl, gb, freq=408, spind=-2.9):
-    table = read_fits(fits)
-    print("gl  gb  freq  temp")
-    print("{} {} {} {:.3f}".format(gl, gb, freq, SkyTemp(gl, gb, table).get_temp(freq, spind)))
-        
+    print("gl      gb     freq  temp")
+    temp = SkyTemp(gl, gb, fits).get_temp(freq, spind)
+    print("{:<7.2f} {:<6.2f} {:<5.0f} {:.3f}".format(gl, gb, freq, temp))
+
 
 if __name__ == "__main__":
     main(**proc_args())
